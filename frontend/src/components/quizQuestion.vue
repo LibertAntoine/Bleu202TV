57c5be1e44c399c4eb970ec74c528366954a079d
Antoine Libert <duncan-arrakis@hotmail.fr>
2020-06-06 19:15:45 +0200

    CREATE quiz qnd quizQuestion component

---
+++ frontend/src/components/quizQuestion.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="">
+        <h2>{{ question }}</h2>
+        <input type="text" id="name" name="name">
+    </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+    },
+    props: {
+        question: {
+            type: [String],
+            required: true
+        }
+    },
+    methods: {
+    }
+}
+</script>
+
+<style>
+</style>
---
+++ frontend/src/views/quiz.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <p>Hello Word</p>
+  </div>
+</template>
+
+<script>
+//import quizQuestion from '@/components/quizQuestion'
+
+export default {
+  name: 'quiz',
+  components: {
+      //quizQuestion
+  }
+}
+</script>
+
+<style>
+#app {
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
