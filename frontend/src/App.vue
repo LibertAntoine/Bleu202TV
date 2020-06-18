698c6ceb73ca065f0d52cd514091da3217f44514
AntoineLibert <duncan-arrakis@hotmail.fr>
2020-06-18 10:19:06 +0200

    Add somes visuals tests

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/CompagnieBleu202.png">
+    <div id="TV-group">
+      <img id="TV" alt="Vue logo" src="./assets/TV.png">
+      <div id="screen" v-on:click="hello" ></div>
+    </div>
   </div>
 </template>
 
@@ -8,14 +11,52 @@
 export default {
   name: 'App',
   components: {
+  },
+  methods: {
+    hello() {
+      console.log("hello");
+    }
   }
 }
 </script>
 
-<style>
+<style lang="scss" >
+
+
+#TV-group {
+  padding-top : 40px;
+  height : 90%;
+}
+
+#TV {
+  position: absolute;
+  right : 12%;
+  height: 90%;
+  z-index: 1;
+}
+
+#screen {
+  position: absolute;
+  margin-right : 550px;
+  right : 12%;
+  top : 25%;
+  background-image : url("./assets/gif-anime.gif");
+  background-repeat: none;
+  height : 50%;
+  width : 1900px;
+  z-index: 2;
+}
+
 #app {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  height : 100%;
+}
+
+@media (max-width: 830px) {
+  #TV {
+    display: none;
+  }
 }
 </style>
