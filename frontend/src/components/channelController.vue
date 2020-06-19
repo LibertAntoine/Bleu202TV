044f1736603160d423794ec8572b6df33954ce58
Antoine Libert <duncan-arrakis@hotmail.fr>
2020-06-19 12:09:15 +0200

    Create the channel controller menu.

---
+++ frontend/src/components/channelController.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="channelController">
+    <div id="channelTest">
+        <img class="BoutonChannel" alt="Button Channel" src="@/assets/Bouton_Appuye.png">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'channelController',
+  components: {
+  },
+  watch: {
+  },
+  mounted: function() {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="scss">
+.BoutonChannel {
+  position: relative;
+  top: 10%;
+  right: 40%; 
+  height: 80%;
+}
+
+#channelTest {
+  z-index: 6;
+  border-radius: 5px;
+  position: relative;
+  height: 100%;
+  width: 100%;
+  transition: background-color 0.3s;
+}
+
+#channelTest:hover {
+    background-color: rgba($color: #ffffff, $alpha: 0.3);
+}
+
+</style>
