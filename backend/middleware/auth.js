de898c646ab3f33cf1a38018701f0721020381b6
Antoine Libert <duncan-arrakis@hotmail.fr>
2020-06-30 15:28:28 +0200

    edit error status for bad authentificaion

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,6 +4,7 @@ const User = require('../models/User');
 module.exports = async (req, res, next) => {
   try {
     const decodedToken = jwt.verify(req.headers.authorization.split(' ')[1], process.env.SECRET_KEY);
+    console.log(decodedToken)
     if (!decodedToken.uniquename) { throw 'Invalid token';}
     const user = await User.findOne({ uniqueName: decodedToken.uniquename})
     if (!user) {
@@ -13,7 +14,7 @@ module.exports = async (req, res, next) => {
       next();
     }
   } catch {
-    res.status(401).json({
+    res.status(203).json({
       error: new Error('Invalid request!')
     });
   }
