72ee33542d488ec5f9150cc0775729a5c3ca21f2
Antoine Libert <duncan-arrakis@hotmail.fr>
2020-06-30 15:32:28 +0200

    Create componant to manage audio system

---
+++ frontend/src/components/audio/ambianceManager.vue
@@ -0,0 +1,66 @@
+<template>
+    <div> 
+      </div>
+</template>
+
+<script>
+export default {
+  name: 'ambianceManager',
+  props: {
+    CurrentCanal : {type: String, required : true},
+    Mute : {type: Boolean, default : false},
+    TotalMute : {type: Boolean, default : false}
+  },
+  watch: {
+    CurrentCanal : function() {this.toggleAmbiance()},
+    Mute : function() {this.toggleMute()},
+    TotalMute : function() {this.toggleTotalMute()}
+  },
+  data() {
+    return {
+      defaultAudio : new Audio(require('@/audio/RomanPhotoAmbianceSalon.mp3')),
+      currentAmbiance: null
+    }
+  },
+  mounted() {
+    this.$datas[0].ambiance.loop = true;
+    this.$datas[1].ambiance.loop = true;
+    this.$datas[2].ambiance.loop = true;
+    this.$datas[3].ambiance.loop = true;
+    this.$datas[4].ambiance.loop = true;
+    this.defaultAudio.loop = true;
+    this.currentAmbiance = this.defaultAudio
+  },
+  methods: {
+    toggleAmbiance() {
+      this.currentAmbiance.pause()
+      this.currentAmbiance.currentTime = 0;
+      if(this.CurrentCanal < 0 || this.CurrentCanal > 4 || !this.$logStore.state.connected) {
+        this.currentAmbiance = this.defaultAudio
+      } else {
+        this.currentAmbiance = this.$datas[this.CurrentCanal].ambiance
+      }
+      if(this.TotalMute == true) {this.currentAmbiance.volume = 0} 
+      else {this.currentAmbiance.volume = 1}
+      this.currentAmbiance.play()
+    },
+    toggleMute() {
+      if(this.Mute) {
+        this.currentAmbiance.volume = 0
+      } else if (!this.TotalMute) {
+        this.currentAmbiance.volume = 1
+      }
+    }, 
+    toggleTotalMute() {
+      if(this.TotalMute) {
+        this.currentAmbiance.volume = 0
+      } else {
+        this.currentAmbiance.volume = 1
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" >
+</style>
---
+++ frontend/src/components/audio/bruitageManager.vue
@@ -0,0 +1,31 @@
+<template>
+    <div> 
+      </div>
+</template>
+
+<script>
+export default {
+  name: 'bruitageManager',
+  props: {
+    TotalMute : {type: Boolean, default : false}
+  },
+  data() {
+    return {
+      zap : new Audio(require('@/audio/zap.mp3')),
+    }
+  },
+  mounted() {
+    this.$datas[0].ambiance.loop = true;
+  },
+  methods: {
+    playZap() {
+        if(!this.TotalMute) {
+            this.zap.play()
+        }
+    }
+  }
+}
+</script>
+
+<style lang="scss" >
+</style>
